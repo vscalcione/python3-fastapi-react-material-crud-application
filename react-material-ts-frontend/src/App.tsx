78ae314fcdae9721c41704b7e5132765b97c0571
DIR\vincenzo.scalcione <vincenzo.scalcione@accenture.com>
2021-07-08 12:50:50 +0200

    Refactoring code

--- react-material-ts-frontend/src/App.tsx
+++ react-material-ts-frontend/src/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { NavbarComponent } from './components/navbar/NavbarComponent';
+import { NavbarComponent }gi from './components/navbar/NavbarComponent';
 import { FormComponent } from './components/form/FormComponent';
 
 function App() {
   return (
     <div>
       <NavbarComponent />
-      <FormComponent></FormComponent>
+      <FormComponent />
     </div>
   );
 }
